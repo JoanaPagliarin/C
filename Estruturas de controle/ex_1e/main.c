f83de09425250727d9eba2bbee4ecce9237bced4
Joana Pagliarin <109366002+JoanaPagliarin@users.noreply.github.com>
2022-08-16 15:33:01 -0300

    terminado 1e

--- Estruturas de controle/ex_1e/main.c
+++ Estruturas de controle/ex_1e/main.c
@@ -1,92 +1,82 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//Dado um dígito, escrever o mesmo em código Morse.
-/*
-    1 . _ _ _ _ 1 ponto e 4 traços
-    2 . . _ _ _ 2 pontos e 3 traços
-    3 . . . _ _ 3 pontos e 2 traços
-    4 . . . . _ 4 pontos e 1 traço
+/*Dado um dÃ­gito, escrever o mesmo em cÃ³digo Morse.
+    1 . _ _ _ _ 1 ponto e 4 traÃ§os
+    2 . . _ _ _ 2 pontos e 3 traÃ§os
+    3 . . . _ _ 3 pontos e 2 traÃ§os
+    4 . . . . _ 4 pontos e 1 traÃ§o
     5 . . . . . 5 pontos
-    6 _ . . . . 1 traço e 4 pontos
-    7 _ _ . . . 2 traços e 3 pontos
-    8  _ _ _ . . 3 traços e 2 pontos
-    9  _ _ _ _ . 4 traços e 1 ponto
-    0 _ _ _ _ _  5 traços
-
+    6 _ . . . . 1 traÃ§o e 4 pontos
+    7 _ _ . . . 2 traÃ§os e 3 pontos
+    8  _ _ _ . . 3 traÃ§os e 2 pontos
+    9  _ _ _ _ . 4 traÃ§os e 1 ponto
+    0 _ _ _ _ _  5 traÃ§os
 */
-
 
 int main()
 {
-    int n[3], i = 0, j = 0;
-    int ponto, traco, aux = 0;
-    int digitos[10] = {1,2,3,4,5,6,7,8,9,0};
+char str[1000];
+int digitos = 0, ponto = 0, traco = 0, n;
 
+printf("digite um numero:  ");
+scanf("%s", str);
 
-printf("digite um numero de 3 digitos:  ");
-while (i < 3)
+digitos = strlen(str);
+
+for(int i = 0; i < digitos; i++)
 {
-    scanf("%1d", &n[i]);
-    i++;
-}
-
-for (i = 0; i < 3; i++)
-{
-    for (j = 0; j < 10; j++)
+    switch(str[i])
     {
-        if (n[i] == digitos[j])
-        {
-            while (digitos[j] <= 5)
-            {
-                printf("%d = ", digitos[j]);
+        case '1':
+        printf("1 = . _ _ _ _");
+        printf("\n");
+        break;
 
-                for(ponto = 0; ponto <= j; ponto++)
-                {
-                    printf(". ");
-                }
+        case '2':
+        printf("2 = . . _ _ _");
+        printf("\n");
+        break;
 
-                for (traco = 0; traco < 5 - ponto; traco++)
-                {
-                    printf("- ");
-                }
+        case '3':
+        printf("3 =  . . . _ _ ");
+        printf("\n");
+        break;
 
-                printf("\n");
-            }
+        case '4':
+        printf("4 = . . . . _ ");
+        printf("\n");
+        break;
 
-            while ( digitos[j] > 5 && digitos[j] < 10)
-            {
-                printf("%d = ", digitos[j]);
+        case '5':
+        printf("5 = . . . . .");
+        printf("\n");
+        break;
 
-                for(traco = 0; traco < digitos[j] - j + aux; traco++)
-                {
-                    printf("- ");
-                }
+        case '6':
+        printf("6 = _ . . . .");
+        printf("\n");
+        break;
 
-                for (ponto = 0; ponto < 5 - traco; ponto++)
-                {
-                    printf(". ");
-                }
+        case '7':
+        printf("7 = _ _ . . . ");
+        printf("\n");
+        break;
 
-                printf("\n");
-            }
+        case '8':
+        printf("8 =  _ _ _ . . ");
+        printf("\n");
+        break;
 
-            if (digitos[j] == 0)
-            {
-                printf("0 = ");
+        case '9':
+        printf("9 =  _ _ _ _ . ");
+        printf("\n");
+        break;
 
-                    for (traco = 0; traco < 5; traco++)
-                    {
-                        printf("- ");
-                    }
-            }
-        }
-
-        else
-        {
-            continue;
-        }
-
+        case '0':
+        printf("0 = _ _ _ _ _");
+        printf("\n");
+        break;
     }
 }
 
