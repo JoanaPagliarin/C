55da1826c952d5beed3522ca7d51b2133b1f6320
joana <joanapagliarin5@gmail.com>
2022-09-03 11:41:55 -0300

    menu simples concluido; incrementar

--- 8. 1 Projeto Cinema/ex_Menu_Login_Cadastro/main.c
+++ 8. 1 Projeto Cinema/ex_Menu_Login_Cadastro/main.c
@@ -7,15 +7,12 @@
 
 struct Cliente
 {
-    char nome[50];
-    char cpf[14];
-    int ocupado;
+   int ocupado;
 };
 
 void inicio (struct Cliente clientes[], int n);
 void exibe_poltronas (struct Cliente clientes[], int n);
 void escolhe_poltrona (struct Cliente clientes[], int n);
-void cadastro_cliente (struct Cliente clientes[], int i);
 
 int main()
 {
@@ -43,17 +40,14 @@ void exibe_poltronas (struct Cliente clientes[], int n)
         for(int i = 0; i < 6; i++)
         {
             if (clientes[poltrona].ocupado == 0)
-            {
-                printf("%d ", poltrona);
-                poltrona++;
-            }
+            printf("%d ", poltrona);
 
             else
-            {
-                printf("   ");
-                poltrona++;
-            }
+            printf("   ");
+
+            poltrona++;
         }
+
         printf("\n");
     }
 }
@@ -65,62 +59,46 @@ void escolhe_poltrona (struct Cliente clientes[], int n)
     int poltrona;
 
     do{
-        for(int i = 0; i < n; i++)
-        {
-            printf("Digite 'c' para comprar, 'r' para reservar, 'x' para cancelar reserva ou 's' para sair:  ");
-            scanf("%c",&resposta);
-
-            switch (resposta)
-            {
-            case 'c':
+            for(int i = 0; i < n; i++)
                 {
-                    cadastro_cliente(clientes, i);
-                    printf("Digite uma poltrona para compra:  ");
-                    scanf("%d", &poltrona);
-                    clientes[poltrona].ocupado = 1;
-                    exibe_poltronas(clientes, n);
-                    break;
+                    printf("Digite 'c' para comprar, 'r' para reservar, 'x' para cancelar reserva ou 's' para sair:  ");
+                    scanf("%c",&resposta);
+
+                    switch (resposta)
+                    {
+                        case 'c':
+                            {
+                                printf("Digite uma poltrona para compra:  ");
+                                scanf("%d", &poltrona);
+                                clientes[poltrona].ocupado = 1;
+                                exibe_poltronas(clientes, n);
+                                break;
+                            }
+
+                        case 'r':
+                            {
+                                printf("Digite uma poltrona para reserva:  ");
+                                scanf("%d", &poltrona);
+                                clientes[poltrona].ocupado = 1;
+                                exibe_poltronas(clientes, n);
+                                break;
+                            }
+
+                        case 'x':
+                            {
+                                printf("Digite uma poltrona para cancelar a reserva:  ");
+                                scanf("%d", &poltrona);
+                                clientes[poltrona].ocupado = 0;
+                                exibe_poltronas(clientes, n);
+                                break;
+                            }
+
+                        case 's':
+                            {
+                                printf("Tchau!");
+                            }
+                    }
                 }
-
-            case 'r':
-                {
-                    cadastro_cliente(clientes, i);
-                    printf("Digite uma poltrona para reserva:  ");
-                    scanf("%d", &poltrona);
-                    clientes[poltrona].ocupado = 1;
-                    exibe_poltronas(clientes, n);
-                    break;
-                }
-
-            case 'x':
-                {
-                    printf("Digite uma poltrona para cancelar a reserva:  ");
-                    scanf("%d", &poltrona);
-                    clientes[poltrona].ocupado = 0;
-                    exibe_poltronas(clientes, n);
-                    break;
-                }
-
-            case 's':
-                {
-                    printf("Tchau!");
-                }
-        }
-    }
-
-
-    }while(resposta != 's');
+    } while (resposta != 's');
 }
 
-void cadastro_cliente (struct Cliente clientes[], int i)
-{
-    fflush(stdin);
-    printf("Digite seu nome:    ");
-    gets(clientes[i].nome);
-
-    fflush(stdin);
-    printf("\nDigite seu cpf:    ");
-    gets(clientes[i].cpf);
-
-    fflush(stdin);
-}
