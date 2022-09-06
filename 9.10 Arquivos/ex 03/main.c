3c9778c89f0d1ba8e406334a25ec2598a08c6b6c
joana <joanapagliarin5@gmail.com>
2022-09-06 20:17:29 -0300

    finalizado

---
+++ 9.10 Arquivos/ex 03/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define TAM 20
+
+/*receber arquivo de texto e falar quantas letras sao vogais*/
+
+int main()
+{
+    char str[TAM];
+    int i = 0, vogais = 0;
+    FILE *arq = fopen("arq.txt", "w");
+    printf("String:   ");
+    fgets(str, TAM, stdin);
+
+    while(i < strlen(str))
+    {
+        fputc(str[i], arq);
+
+        if(str[i] == 'A' ||
+           str[i] == 'a' ||
+           str[i] == 'E' ||
+           str[i] == 'e' ||
+           str[i] == 'I' ||
+           str[i] == 'i' ||
+           str[i] == 'O' ||
+           str[i] == 'o' ||
+           str[i] == 'U' ||
+           str[i] == 'u')
+            vogais++;
+
+        i++;
+    }
+
+    fclose(arq);
+
+    printf("Qntdd de vogais: %d ", vogais);
+
+    return 0;
+}
